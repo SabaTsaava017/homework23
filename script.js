69be1a00a69b619bb7519b201e591fc613d46d04
SabaTsaava017 <sabacaava7@gmail.com>
2023-05-18 20:06:54 +0400

    add script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+let string = "Saba"
+let number = 50;
+let boolean = false;
+
+
+let a = 15;
+let b = 20;
+let c = 50;
+let d = a + b + c;
+console.log(d);
+
+ a = 100;
+ b = 20;
+ c  = a / b;
+console.log(c);
+
+a = 200;
+b = 55;
+c = 25;
+d = a - b - c;
+console.log(d);
+
+a = 25;
+b = 5;
+c = 2;
+d = a * b * c;
+console.log(d);
+
+
+let firstName = "Saba"
+let lastName = "Tsaava"
+let age = 19;
+let Profession = "Businassman"
+let City = "Tbilisi"
+console.log(" My name is " + firstName + " My lastName is " + lastName + " My am " + age + " years old " )
